005ab19323756dee2334bfc8a3808118285b4a53
Ukech <elephantnose@fish.com>
2025-04-14 11:27:26 +0200

    Added mconfig abstract class for acquisition metho d, started working on Small_inclusion class and folder to finally start working on far field appro ximations

---
+++ cfg/mconfig.py
@@ -0,0 +1,79 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+class mconfig:
+    __src : np.ndarray
+    __rcv : np.ndarray
+
+    _Ng : int
+    _center : np.ndarray
+
+    def __init__(self, Ng, center):
+        self._Ng = Ng
+        self._center = center
+        
+    _Ns : int
+    _Nr : int
+    _Ns_total : int
+    _Nr_total : int
+    _data_dim : int
+
+    #Getter methods
+
+    def get_Ns(self):
+        return self.__src.shape[1]
+    def get_Nr(self):
+        return self.__rcv.shape[1]
+    def get_Ns_total(self):
+        return self._Ns* self._Ng
+    def get_Nr_total(self):
+        return self._Nr*self._Ng
+    def get_data_dim(self):
+        return self._Ns * self._Nr * self._Ng
+    #Utility functions
+    def group(self, g):
+        #Coordinates of sources and receivers of the g-th group
+        src = self.__src[:,g]
+        rcv = self.__rcv[:,g]
+        return src, rcv
+    def src_query(self,s):
+        #Return the group index of a source s
+        if not isinstance(s, int) or s > self._Ns_total or s < 1:
+            raise ValueError("Non-scalar source index or source index out of range")
+        gid = math.floor((s-1) / self._Ns) +1
+        sid = s- (gid-1) * self._Ns
+        return gid, sid
+
+    def src(self, sidx):
+        #Coordinates of sources in sidx
+        r = np.zeros((2, len(sidx)))
+        for s in range(len(sidx)):
+            gid, sid = self.src_query(sidx[s])
+            toto = self.__src[:, gid]
+            r[:,s] = toto[:, sid]
+        return r   
+    def rcv(self,s):
+        #Coordinates of receivers corresponding to s-th source
+        gid, _ = self.src_query(s)
+        return self.__rcv[:,gid]
+    def all_src(self):
+        sr = np.array([])
+        for i in range(self._Ng):
+            sr = np.hstack((sr,self.__src[:,i]))
+        return sr
+    def all_rcv(self):
+        rc = np.array([])
+        for i in range(self._Ng):
+            rc = np.hstack((rc, self.__rcv[:,i]))
+        return rc
+    
+    def plot(self, *args, **kwargs):
+        for g in range(self._Ng):
+            src, rcv = self.group(g)
+            plt.plot(src[0, :], src[1, :], 'x')
+            plt.plot(rcv[0, :], rcv[1, :], 'o')
+        plt.legend()
+        plt.axis('equal')
+        plt.show()
