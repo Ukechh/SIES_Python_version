aef7ba49516abbca96ef4f7305301dcc54c27dec
Ukech <elephantnose@fish.com>
2025-04-16 11:52:02 +0200

    Added Coincided, Circular class definitions, Next step is to verify if this is enough to define a far field expansion method and if so do it, otherwise, building the wavelet file might be necesary

--- cfg/mconfig.py
+++ cfg/mconfig.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from numpy import pi
 
 
 class mconfig:
@@ -77,10 +78,29 @@ class mconfig:
         plt.legend()
         plt.axis('equal')
         plt.show()
+    
+    @staticmethod
+    def src_rcv_circle(Na,N0, R0, Z, theta, aov = 2 * np.pi):
+        Xs = np.zeros((2, N0*Na))
+        Thetas = np.zeros((1, N0*Na))
+        Xsblock = np.empty(Na, dtype=object)
+
+        for n in range(Na):
+            tt0 = (n / Na) * aov
+            tt = tt0 + (np.arange(N0) / N0 ) * theta
+
+            rr = R0 * np.vstack((np.cos(tt), np.sin(tt)))
+            Xs[:,(n*N0):(n+1)*N0] = rr
+            Thetas[(n*N0):((n+1)*N0)] = tt
+
+            Xsblock = rr + Z.reshape(2,1)
+
+        Xs += Z.reshape(2, 1)
+
+        return Xs, Thetas, Xsblock
 
 class Concentric(mconfig):
     nbDirac : int
-    neutSrc : np.ndarray
     neutCoeff : np.ndarray
     neutRad : float
 
@@ -90,7 +110,7 @@ class Concentric(mconfig):
 
     def __init__(self, Z, Rs, Ns, Rr, Nr, viewmode = np.array([1,2*np.pi, 2*np.pi]), grouped = 0, neutCoeff = np.ones(1), neutRad=0.01):
         self.neutRad = neutRad
-        if len(neutCoeff) <= 1:
+        if neutCoeff.shape[0] <= 1:
             neutCoeff = np.ones(1)
         else:
             if neutCoeff.sum() != 0 or abs(neutCoeff).max() ==0:
@@ -100,8 +120,8 @@ class Concentric(mconfig):
         theta = viewmode[1]
         aov = viewmode[2]
 
-        Xs, _, Xscell = src_rcv_circle(Na, Ns, Rs, Z, theta, aov)
-        Xr, _, Xrcell = src_rcv_circle(Na, Nr, Rr, Z, theta, aov)
+        Xs, _, Xsblock = mconfig.src_rcv_circle(Na, Ns, Rs, Z, theta, aov)
+        Xr, _, Xrblock = mconfig.src_rcv_circle(Na, Nr, Rr, Z, theta, aov)
 
         self._center = Z
         self.radius_src = Rs
@@ -109,8 +129,8 @@ class Concentric(mconfig):
 
         if grouped:
             self.Ng = Na
-            self.__src = Xscell
-            self.__rcv = Xrcell
+            self.__src = Xsblock
+            self.__rcv = Xrblock
         else:
             self.Ng = 1
             self.__src = Xs
@@ -120,4 +140,67 @@ class Concentric(mconfig):
             self.equispaced = 1
     
     def plot(self, *args, **kwargs):
-        pass
+        super().plot(*args, **kwargs)
+        plt.plot(self._center[0], self._center[1], 'r*')
+    
+    def get_nbDirac(self):
+        return self.neutCoeff.shape[0]
+    
+    @property
+    def neutSrc(self, s=0):
+        psrc = self.__src[s]
+        if self.nbDirac == 1:
+            return psrc
+        else:
+            r = np.zeros((2,self.nbDirac))
+            L = self.radius_src * self.neutRad
+            toto = psrc - self._center
+            q = np.array([toto[1], -toto[0]])
+            q = L * (q / np.linalg.norm(q))
+            for n in range(self.nbDirac):
+                r[:,n] = psrc + n * (q / self.nbDirac)
+            return r
+
+class Planewave(mconfig):
+    radius_src = 1
+    def __init__(self, Z, Rr, Nr, Ns, viewmode=np.array([1, 2 * np.pi, 2 * np.pi]), grouped=0):
+        Na = viewmode[0]
+        theta = viewmode[1]
+        aov = viewmode[2]
+        Xs,_, Xsblock = mconfig.src_rcv_circle(Na, Ns, 1, np.ones(2), theta, aov)
+        Xr,_, Xrblock = mconfig.src_rcv_circle(Na, Nr, Rr, Z, theta, aov)
+
+        self._center = Z
+        self.radius_rcv = Rr
+
+        if grouped:
+            self._Ng = Na
+            self.__src = Xsblock
+            self.__rcv = Xrblock
+        else:
+            self._Ng = 1
+            self.__src = Xs
+            self.__rcv = Xr
+        if self._Ng == 1 and theta == 2*np.pi:
+            self.equispaced = 1
+    
+    def plot(self, *args, **kwargs):
+        for g in range(self._Ng):
+            src, rcv = self.group(g)
+            src = self.radius_rcv * src + self._center
+            plt.plot(src[0, :], src[1, :], 'x')
+
+            rcv = self.radius_src*rcv
+            plt.plot(rcv[0, :], rcv[1, :], 'o')
+        plt.plot(self._center[0], self._center[1], 'r*')
+        plt.axis('equal')
+        plt.show()
+
+#TO DO: Interior ROI, Interior rcv, Fish circle mconfig classes,
+#Need to define wavelet classes in order to properly buil these...
+
+class Coincided(Concentric):
+    def __init__(self, Z, Rs, Ns, viewmode=np.array([1, 2 * np.pi, 2 * np.pi]), grouped=0, neutCoeff=np.empty(1), neutRad=0.01):
+        super().__init__(Z, Rs, Ns, Rs, Ns, viewmode, grouped, neutCoeff, neutRad)
+
+ 
